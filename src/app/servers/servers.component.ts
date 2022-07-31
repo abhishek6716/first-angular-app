c42a8bfb2526389791ff6ded860f6505921905f1
abhishek6716 <abhisheksingh6716@gmail.com>
2022-07-31 09:27:57 +0530

    basics of structural directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No Server Was Created!';
-  serverName: string = '';
+  serverName: string = 'TestServer';
+  serverCreated: boolean = false;
+  servers: Array<string> = ['TestServer1', 'TestServer2'];
 
   constructor() {
     setTimeout(() => {
@@ -19,6 +21,8 @@ export class ServersComponent implements OnInit {
   ngOnInit() {}
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus =
       'Server Was Created! Server Name is ' + this.serverName;
   }
